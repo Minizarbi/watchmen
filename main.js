b8edfd4eb0f0fc22e5690e2e9705025534645fc4
Marcfr <marcalquier81@gmail.com>
2016-03-30 20:24:36 +0200

    ajout cache incomplet

--- main.js
+++ main.js
@@ -89,6 +89,10 @@ function init(){
     scene.add( lumiere );
 
     renderer.render( scene, camera );
+	
+	//création du fichier de sauvegarde
+	window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+	window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
 }
 
 function onDocumentMouseDown( event ) {
@@ -150,4 +154,47 @@ function animate(){
 	sphere.position.y=oldY+speedY;
     }
     renderer.render( scene, camera );
+}
+
+function onInitFs(fs) {
+   fs.root.getFile('tutorielsenfolie.txt', {create: true}, writeFile, errorHandler);
+}
+
+function errorHandler(e) {
+   var msg = '';
+   switch (e.code) {
+      case FileError.QUOTA_EXCEEDED_ERR:
+         msg = 'QUOTA_EXCEEDED_ERR';
+         break;
+      case FileError.NOT_FOUND_ERR:
+         msg = 'NOT_FOUND_ERR';
+         break;
+      case FileError.SECURITY_ERR:
+         msg = 'SECURITY_ERR';
+         break;
+      case FileError.INVALID_MODIFICATION_ERR:
+         msg = 'INVALID_MODIFICATION_ERR';
+         break;
+      case FileError.INVALID_STATE_ERR:
+         msg = 'INVALID_STATE_ERR';
+         break;
+      default:
+         msg = 'Unknown Error';
+         break;
+   };
+   alert('Error: ' + msg);
+}
+
+function writeFile(fileEntry) {
+   fileEntry.createWriter(function(fileWriter) {
+      fileWriter.onwriteend = function(e) {
+         alert('OK.');
+      };
+      fileWriter.onerror = function(e) {
+         alert('Error: ' + e.toString());
+      };
+      // On crée un objet Blob pour écrire dans le fichier.
+      var data = new Blob(['Tutoriels en folie'], {type: 'text/plain'});
+      fileWriter.write(data);
+   }, errorHandler);
 }
